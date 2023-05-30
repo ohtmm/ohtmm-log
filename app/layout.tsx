2f01dc977c227bdcf2ed6c7888e4ea23e07e36e2
jiyoung <cwd05011@gmail.com>
2023-05-30 13:06:05 +0900

    design: 메인 한글 폰트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import './globals.css';
-import { Montserrat } from 'next/font/google';
+import { Montserrat, Noto_Sans_KR } from 'next/font/google';
 
 export const metadata = {
   title: 'ohtmm.log',
@@ -9,11 +9,15 @@ export const metadata = {
 };
 
 const montserrat = Montserrat({ weight: ['300', '600', '800'], subsets: ['latin'] });
+const notoSansKr = Noto_Sans_KR({ weight: ['300', '400', '700'], subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body className={`bg-white min-h-screen ${montserrat.className}`}>
+      <body
+        className={`bg-white min-h-screen`}
+        style={{ fontFamily: `${montserrat.style.fontFamily}, ${notoSansKr.style.fontFamily}` }}
+      >
         <div className="w-4/5 md:w-11/12 mx-auto min-h-fit">
           <Navbar />
           {children}
